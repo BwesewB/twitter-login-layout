9847a7c5d7939333b21562a471aeccd21896f8d2
BwesewB <144720614+BwesewB@users.noreply.github.com>
2024-01-08 23:50:15 -0800

    Add files via upload

---
+++ components/Footercomponent/index.js
@@ -0,0 +1,28 @@
+import styles from './Footer.module.css'
+export default function Footer(){
+
+    return(
+        <footer className={styles.footer}>
+            <a class="fakeLink" href="">About</a>
+            <a class="fakeLink" href="">Download the X app</a>
+            <a class="fakeLink" href="">Help Center</a>
+            <a class="fakeLink" href="">Terms of Service</a>
+            <a class="fakeLink" href="">Privacy Policy</a>
+            <a class="fakeLink" href="">Cookie Policy</a>
+            <a class="fakeLink" href="">Accessibility</a>
+            <a class="fakeLink" href="">Ads info</a>
+            <a class="fakeLink" href="">Blog</a>
+            <a class="fakeLink" href="">Status</a>
+            <a class="fakeLink" href="">Careers</a>
+            <a class="fakeLink" href="">Brand Resources</a>
+            <a class="fakeLink" href="">Advertising</a>
+            <a class="fakeLink" href="">Marketing</a>
+            <a class="fakeLink" href="">X for Business</a>
+            <a class="fakeLink" href="">Developers</a>
+            <a class="fakeLink" href="">Directory</a>
+            <a class="fakeLink" href="">Settings</a>
+            <p>© 2024 X Corp.</p>
+      </footer>
+    )
+    
+}
